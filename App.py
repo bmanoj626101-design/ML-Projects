22b2a89685c0ea7b1d5ced1a7aa4f97da1bdc437
bmanoj626101-design <b.manoj626101@gmail.com>
2026-07-02 09:41:09 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 
 # Load model
-with open("VS code/cdk.pkl", "rb") as f:
+with open("cdk.pkl", "rb") as f:
     model = pickle.load(f)
 
 st.title("🍇 Raisin Type Classifier")
